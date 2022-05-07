78e0fb9d5b032407f7e80f5c7cfa6e8dfa132204
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-05-07 16:28:41 +0500

    Load and Cache Image Picasso

--- app/src/main/java/com/shevy/kotlinyoutubelbta/MainAdapter.kt
+++ app/src/main/java/com/shevy/kotlinyoutubelbta/MainAdapter.kt
@@ -3,10 +3,13 @@ package com.shevy.kotlinyoutubelbta
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
 
-class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>() {
+class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolder>() {
 
     val videoTitle = listOf("First", "Second", "3rd", "MORE TITTLE")
 
@@ -21,7 +24,15 @@ class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder
         //val videoTitles = videoTitle[position]
         val video = homeFeed.videos[position]
         holder.view.findViewById<TextView>(R.id.textView_video_title).text = video.name
+        holder.view.findViewById<TextView>(R.id.textView_channel_name).text =
+            video.channel.name + " • " + "20K Views\n4 days ago"
 
+        val thumbnailImageView = holder.view.findViewById<ImageView>(R.id.imageView_video_thumbnail)
+        Picasso.get().load(video.imageUrl).into(thumbnailImageView)
+
+        val channelProfileImageView =
+            holder.view.findViewById<CircleImageView>(R.id.imageView_channel_profile)
+        Picasso.get().load(video.channel.profileimageUrl).into(channelProfileImageView)
     }
 
     override fun getItemCount(): Int {
@@ -29,5 +40,5 @@ class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder
     }
 }
 
-class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+class CustomViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 }
